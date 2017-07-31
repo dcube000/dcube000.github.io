6da9f7a1ca681ce36ef45a572a5e3be6a6243cd3
dcube000 <dcube000.gmail.com>
2017-07-31 23:12:59 +0530

    analytics

--- lets.js
+++ lets.js
@@ -1,5 +1,20 @@
 // Get the modal
 window.onload = function(){
+    
+    
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-89580205-2', 'auto');
+  ga('send', 'pageview');
+
+
+    
+    
+    
     var modal = document.getElementById("myModal");
     // Get the image and insert it inside the modal - use its "alt" text as a caption
     var img = document.getElementById('myImg');
